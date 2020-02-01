7fb3cb25656b48bfa4c47faa41c4716910a3ab47
Andrew Patrick <andrew.prsilva@gmail.com>
2020-02-01 13:50:16 -0300

    mongo conection created

--- src/main/java/com/workshop/resources/UserResource.java
+++ src/main/java/com/workshop/resources/UserResource.java
@@ -1,27 +1,26 @@
 package com.workshop.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.workshop.domain.User;
+import com.workshop.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll() {
-		User andrew = new User("1", "Andrew", "andrew@gmail.com");
-		User patrick = new User("2", "Patrick", "patrik@gmail.com");
-
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(andrew, patrick));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
