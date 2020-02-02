730d8659c15a85e9fbc2fcebe14d06f5854005c6
Andrew Patrick <andrew.prsilva@gmail.com>
2020-02-01 22:13:22 -0300

    POST method

--- src/main/java/com/workshop/services/UserService.java
+++ src/main/java/com/workshop/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshop.domain.User;
+import com.workshop.dto.UserDTO;
 import com.workshop.repository.UserRepository;
 import com.workshop.services.exception.ObjectNotFoundException;
 
@@ -19,10 +20,18 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found !"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
