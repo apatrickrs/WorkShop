b3446a6e1e414350aabd6e5f5a67cf8d87aaee99
Andrew Patrick <andrew.prsilva@gmail.com>
2020-02-03 18:57:34 -0300

    Get User post by id

---
+++ src/main/java/com/workshop/services/PostService.java
@@ -0,0 +1,25 @@
+package com.workshop.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.workshop.domain.Post;
+import com.workshop.repository.PostRepository;
+import com.workshop.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Optional<Post> findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Not found");
+		}
+		return user;
+	}
+}
