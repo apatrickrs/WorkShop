374ba1bbdc08f94f41b9af145341398f95066e0a
Andrew Patrick <andrew.prsilva@gmail.com>
2020-02-03 20:39:14 -0300

    Add comments at posts

--- src/main/java/com/workshop/config/Instantiation.java
+++ src/main/java/com/workshop/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.workshop.domain.Post;
 import com.workshop.domain.User;
 import com.workshop.dto.AuthorDTO;
+import com.workshop.dto.CommentDTO;
 import com.workshop.repository.PostRepository;
 import com.workshop.repository.UserRepository;
 
@@ -39,8 +40,15 @@ public class Instantiation implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
 		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu viagem", "São Paulo, abraço", new AuthorDTO(alex));
-		Post post2 = new Post(null, sdf.parse("21/03/2019"), "Partiu viagem", "São Paulo, abraço", new AuthorDTO(alex));
+		Post post2 = new Post(null, sdf.parse("21/03/2019"), "gogo viagem", "RJ, abraço", new AuthorDTO(alex));
 
+		CommentDTO c1 = new CommentDTO("Vai com Deus", sdf.parse("21/03/2019"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveita", sdf.parse("22/03/2019"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Chama no desk", sdf.parse("23/03/2019"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		PostRepository.saveAll(Arrays.asList(post1, post2));
 		
 		alex.getPosts().addAll(Arrays.asList(post1, post2));
